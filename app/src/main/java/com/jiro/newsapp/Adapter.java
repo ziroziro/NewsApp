5c16f2f8feb6f9183f6463c929d5f4c728722e9f
MacBook <macbook@ziro.local>
2021-01-25 14:54:32 +0700

    Initial commit

---
+++ app/src/main/java/com/jiro/newsapp/Adapter.java
@@ -0,0 +1,71 @@
+package com.jiro.newsapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.helper.widget.Layer;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jiro.newsapp.Models.Articles;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    Context context;
+    List<Articles> articles;
+
+    public Adapter(Context context, List<Articles> articles) {
+        this.context = context;
+        this.articles = articles;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Articles a = articles.get(position);
+        holder.tvTitle.setText(a.getTitle());
+        holder.tvSource.setText(a.getSource().getName());
+        holder.tvDate.setText(a.getPublishedAt());
+
+        String imageUrl = a.getUrlToImage();
+
+        Picasso.with(context).load(imageUrl).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle,tvSource, tvDate;
+        ImageView imageView;
+        CardView cardView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.tvTITLE);
+            tvSource = itemView.findViewById(R.id.tvSOURCE);
+            tvDate = itemView.findViewById(R.id.tvDATE);
+            imageView = itemView.findViewById(R.id.img);
+            cardView = itemView.findViewById(R.id.cardView);
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/jiro/newsapp/ApiInterface.java
@@ -0,0 +1,16 @@
+package com.jiro.newsapp;
+
+import com.jiro.newsapp.Models.Headlines;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+}
